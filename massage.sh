d2c9bafe8eaf12b20d54aafc8a3d9345a19f6af6
arungupta09 <arun.gupta1@telus.com>
2022-12-11 12:49:17 +0530

    Added sa

--- massage.sh
+++ massage.sh
@@ -7,7 +7,8 @@ set -u
 PATH=$PATH:/gcloud/bin
 gcloud config set auth/impersonate_service_account "${GOOGLE_IMPERSONATE_SERVICE_ACCOUNT}"
 gcloud config set project "${PROJECT_ID}"
-API=$(gcloud run services describe frontend --region=$1 --format="value(status.url)" --impersonate-service-account="sa-terraform@${PROJECT_ID}.iam.gserviceaccount.com")
+
+API=$(gcloud run services describe frontend --region=$1 --format="value(status.url)")
 stripped=$(echo ${API/https:\/\//})
 sed -i"" -e "s/127.0.0.1:9000/$stripped/" www/js/main.js
 
