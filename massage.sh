c80ad00a7d3df890e7d12903d6494075735d5a86
arungupta09 <arun.gupta1@telus.com>
2022-12-11 13:44:38 +0530

    Removed massage.sh

--- massage.sh
+++ massage.sh
@@ -6,7 +6,6 @@ set -u
 
 PATH=$PATH:/gcloud/bin
 gcloud config set auth/impersonate_service_account "sa-terraform@${PROJECT_ID}.iam.gserviceaccount.com"
-gcloud projects add-iam-policy-binding "${PROJECT_ID}" --member="serviceAccount:sa-terraform@${PROJECT_ID}.iam.gserviceaccount.com" --role="roles/iam.serviceAccountTokenCreator" --condition=None
 
 API=$(gcloud run services describe frontend --region=$1 --format="value(status.url)")
 stripped=$(echo ${API/https:\/\//})
