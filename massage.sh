dc96c38d1444e58de6c5ee20d16d7e7558fde2f7
arungupta09 <arun.gupta1@telus.com>
2022-12-11 12:38:54 +0530

    Added sa

--- massage.sh
+++ massage.sh
@@ -1,4 +1,4 @@
-API=$(gcloud run services describe frontend --region=$1 --format="value(status.url)" --impersonate-service-account="sa-terraform@${_PROJECT_ID}.iam.gserviceaccount.com")
+API=$(gcloud run services describe frontend --region=$1 --format="value(status.url)" --impersonate-service-account="sa-terraform@sinuous-crow-370906.iam.gserviceaccount.com")
 stripped=$(echo ${API/https:\/\//})
 sed -i"" -e "s/127.0.0.1:9000/$stripped/" www/js/main.js
 
