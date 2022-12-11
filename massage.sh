9f5d6e01a10f7acdd54c558483e537f76937215f
arungupta09 <arun.gupta1@telus.com>
2022-12-11 12:20:45 +0530

    Added sa

--- massage.sh
+++ massage.sh
@@ -1,4 +1,4 @@
-API=$(gcloud run services describe frontend --region=$1 --format="value(status.url)")
+API=$(gcloud run services describe frontend --region=$1 --format="value(status.url)" --service-account="cloud-run-sa@${PROJECT_ID}.iam.gserviceaccount.com")
 stripped=$(echo ${API/https:\/\//})
 sed -i"" -e "s/127.0.0.1:9000/$stripped/" www/js/main.js
 
