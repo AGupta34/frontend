70b10a1d8d9b066985a3966b378b3c3f94300f11
arungupta09 <arun.gupta1@telus.com>
2022-12-11 12:28:39 +0530

    Added sa

--- massage.sh
+++ massage.sh
@@ -1,6 +1,4 @@
-gcloud config set auth/impersonate_service_account "${GOOGLE_IMPERSONATE_SERVICE_ACCOUNT}"
-gcloud config set project "${PROJECT_ID}"
-API=$(gcloud run services describe frontend --region=$1 --format="value(status.url)")
+API=$(gcloud run services describe frontend --region=$1 --format="value(status.url)" --impersonate-service-account="cloud-run-sa@${PROJECT_ID}.iam.gserviceaccount.com")
 stripped=$(echo ${API/https:\/\//})
 sed -i"" -e "s/127.0.0.1:9000/$stripped/" www/js/main.js
 
